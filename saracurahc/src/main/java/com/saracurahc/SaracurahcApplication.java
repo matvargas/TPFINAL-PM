372f96d7c24ab2b0610b64551088a85861cf2850
Matheus Vargas <m6.vargas@gmail.com>
2018-11-14 14:10:32 -0200

    Commit inicial da aplicação

---
+++ saracurahc/src/main/java/com/saracurahc/SaracurahcApplication.java
@@ -0,0 +1,12 @@
+package com.saracurahc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaracurahcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaracurahcApplication.class, args);
+    }
+}
