e23b9dfdbf1ebbdc41120c40458a76914f6ff2e7
Matheus Vargas <m6.vargas@gmail.com>
2018-11-21 18:25:50 -0200

    Adição de elementos para tratamento de eventos.

--- saracurahc/src/main/java/com/saracurahc/controllers/rest/ScheduleRestController.java
+++ saracurahc/src/main/java/com/saracurahc/controllers/rest/ScheduleRestController.java
@@ -1,9 +1,25 @@
 package com.saracurahc.controllers.rest;
 
+import com.saracurahc.models.EventDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class ScheduleRestController {
 
+    @Autowired
+    EventDAO eventDAO;
 
+    @RequestMapping(value = "/getAllEvents", method = GET)
+    @ResponseBody
+    public List<Map<String, Object>> getAllEvents(){
+        return eventDAO.getAll();
+    }
 }
---
+++ saracurahc/src/main/java/com/saracurahc/models/EventDAO.java
@@ -0,0 +1,19 @@
+package com.saracurahc.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class EventDAO {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Map<String, Object>> getAll() {
+        return jdbcTemplate.queryForList("select * from Event");
+    }
+}
